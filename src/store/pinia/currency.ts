28c5d7f5b403e3809662bc3878a34b7c028b7815
Ilya Sidorenko <sidorenko@telros.ru>
2024-09-10 11:29:29 +0300

    ADD: add getter resources as array

--- src/store/pinia/currency.ts
+++ src/store/pinia/currency.ts
@@ -1,12 +1,19 @@
 import { defineStore } from "pinia";
-import {IceResource, IronResource, StoneResource, SiliciumResource} from  '../../game/gameClasses/resources'
+import {IceResource, IronResource, StoneResource, SiliciumResource} from  '../../game/gameClasses/Resources'
 export const useUserResourcesStore = defineStore('userResourcesStore', {
     state() {
         return {
-            ice: new IceResource({amount: 100}),
-            iron: new IronResource({amount: 100}),
-            stone: new StoneResource({amount: 100}),
-            silicium: new SiliciumResource({amount: 100}),
+            current: {
+                ice: new IceResource({amount: 100}),
+                iron: new IronResource({amount: 100}),
+                stone: new StoneResource({amount: 100}),
+                silicium: new SiliciumResource({amount: 100}),
+            }
+        }
+    },
+    getters: {
+        list({current: {ice, iron, stone, silicium}}) {
+            return [ice, iron, stone, silicium]
         }
     }
 });
